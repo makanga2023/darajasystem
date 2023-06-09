95af137332a0e6c5e230359b1d9deae369dd1a7b
Antony <antonymakanga88@gmail.com>
2023-06-09 21:00:41 +0300

    second commit

--- daraja-tutorial-master/stk_initiate.php
+++ daraja-tutorial-master/stk_initiate.php
@@ -5,12 +5,12 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'iUHpErGrLDfVNuELY2xDs12vaeGBQpL0'; //Fill with your app Consumer Key
-  $consumerSecret = 'yOugkoPlQpByxU4P'; // Fill with your app Secret
+  $consumerKey = 'xOkXdXYnQbJzGmqwk4AyObL4FHnmg7GM'; //Fill with your app Consumer Key
+  $consumerSecret = 'jzZdM4ubBiwygQ5I'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
-  $BusinessShortCode = '174379';
+  $BusinessShortCode = '600988';
   $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
   
   /*
